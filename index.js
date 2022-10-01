afa6bdcb41177d413dcd6d127c6972a0dc5d53cf
connorbelding <the_connn@hotmail.com>
2022-10-01 01:37:07 -0400

    Added operate function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const operations = {
+  add: "add",
+  subtract: "subtract",
+  multiply: "multiply",
+  divide: "divide",
+};
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +19,23 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operation, numbersObj) {
+  switch (operation) {
+    case operations.add: {
+      return add(numbersObj.num1, numbersObj.num2);
+    }
+    case operations.subtract: {
+      return subtract(numbersObj.num1, numbersObj.num2);
+    }
+    case operations.multiply: {
+      return multiply(numbersObj.num1, numbersObj.num2);
+    }
+    case operations.divide: {
+      return divide(numbersObj.num1, numbersObj.num2);
+    }
+    default:
+      throw new Error("operate switch error");
+  }
 }

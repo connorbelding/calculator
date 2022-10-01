06339f55dcf5c9ece96c64a6e9112f2f806d0f7d
connorbelding <the_connn@hotmail.com>
2022-10-01 02:03:09 -0400

    Added function to generate calculator keys in HTML

--- index.js
+++ index.js
@@ -5,6 +5,73 @@ const operations = {
   divide: "divide",
 };
 
+const calculatorKeys = [
+  {
+    keyChar: 1,
+    type: "number",
+  },
+  {
+    keyChar: 2,
+    type: "number",
+  },
+  {
+    keyChar: 3,
+    type: "number",
+  },
+  {
+    keyChar: "+",
+    type: "operator",
+  },
+  {
+    keyChar: 4,
+    type: "number",
+  },
+  {
+    keyChar: 5,
+    type: "number",
+  },
+  {
+    keyChar: 6,
+    type: "number",
+  },
+  {
+    keyChar: "-",
+    type: "operator",
+  },
+  {
+    keyChar: 7,
+    type: "number",
+  },
+  {
+    keyChar: 8,
+    type: "number",
+  },
+  {
+    keyChar: 9,
+    type: "number",
+  },
+  {
+    keyChar: "*",
+    type: "operator",
+  },
+  {
+    keyChar: "C",
+    type: "operator",
+  },
+  {
+    keyChar: 0,
+    type: "number",
+  },
+  {
+    keyChar: "=",
+    type: "operator",
+  },
+  {
+    keyChar: "/",
+    type: "operator",
+  },
+];
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -39,3 +106,19 @@ function operate(operation, numbersObj) {
       throw new Error("operate switch error");
   }
 }
+
+function generateCalculatorButtons() {
+  const calculatorButtonsContainer =
+    document.getElementById("calculator-buttons");
+
+  calculatorKeys.forEach((key) => {
+    const button = document.createElement("button");
+    button.textContent = key.keyChar;
+    button.addEventListener("pointerdown", () => {
+      console.log(key.keyChar);
+    });
+    calculatorButtonsContainer.appendChild(button);
+  });
+}
+
+generateCalculatorButtons();
